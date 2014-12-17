39ae186cd4845382a46994a988a1691653e95495
Morten Henriksen <mh@gi2.dk>
2014-12-17 14:04:58 +0100

    mbr update versions and fix warnings

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
+  git: 'https://github.com/CollectionFS/Meteor-ui-dropped-event.git',
+  name: 'cfs:dropped-event',
   version: '0.0.7',
   summary: "Add dropped event to Meteor UI Templates"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
+
   api.use(['ui', 'templating'], 'client');
-  api.add_files(['dropped.event.js'], 'client');
+  api.addFiles(['dropped.event.js'], 'client');
 });
